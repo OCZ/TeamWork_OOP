d80cca4dd1afdfcceb7790ea875b2c7978cc1804
re5pectR10 <fc_cska@yahoo.com>
2015-12-16 17:26:58 +0200

    fix collision detector

--- BeerBellyGame/GameObjects/GameObject.cs
+++ BeerBellyGame/GameObjects/GameObject.cs
@@ -13,29 +13,64 @@
         public string AvatarUri { get; set; }
         public Direction IntersectWith(IDrawable obj)
         {
-            if (this.Position.Left <= obj.Position.Left + obj.Size.Width && this.Position.Left + this.Size.Width >= obj.Position.Left
-                && this.Position.Top <= obj.Position.Top + obj.Size.Height && this.Position.Top + this.Size.Height >= obj.Position.Top)
+            if ((this.Position.Left > obj.Position.Left && this.Position.Left < obj.Position.Left + obj.Size.Width)
+                || (this.Position.Left + this.Size.Width > obj.Position.Left && this.Position.Left + this.Size.Width < obj.Position.Left + obj.Size.Width)
+                || (this.Position.Left == obj.Position.Left && this.Position.Left + this.Size.Width == obj.Position.Left + obj.Size.Width))
             {
-                if (this.Position.Top + this.Size.Height <= obj.Position.Top)
+                if (this.Position.Top + this.Size.Height >= obj.Position.Top && this.Position.Top + this.Size.Height < obj.Position.Top + obj.Size.Height)
                 {
                     return Direction.down;
                 }
-                if (this.Position.Top >= obj.Position.Top + obj.Size.Height)
+
+                if (this.Position.Top <= obj.Position.Top + obj.Size.Height && this.Position.Top > obj.Position.Top)
                 {
                     return Direction.up;
                 }
+            }
 
-                if (this.Position.Left + this.Size.Width <= obj.Position.Left)
-                {
-                    return Direction.right;
-                }
-                if (this.Position.Left >= obj.Position.Left + obj.Size.Width)
+            if ((this.Position.Top > obj.Position.Top && this.Position.Top < obj.Position.Top + obj.Size.Height)
+                || (this.Position.Top + this.Size.Height > obj.Position.Top && this.Position.Top + this.Size.Height < obj.Position.Top + obj.Size.Height)
+                || (this.Position.Top == obj.Position.Top && this.Position.Top + this.Size.Height == obj.Position.Top + obj.Size.Height))
+            {
+                if (this.Position.Left <= obj.Position.Left + obj.Size.Width && this.Position.Left > obj.Position.Left)
                 {
                     return Direction.left;
+                }
+
+                if (this.Position.Left + this.Size.Width >= obj.Position.Left && this.Position.Left + this.Size.Width < obj.Position.Left + obj.Size.Width)
+                {
+                    return Direction.right;
                 }
             }
 
             return Direction.none;
         }
+
+        //public Direction IntersectWith(IDrawable obj)
+        //{
+        //    if (this.Position.Left <= obj.Position.Left + obj.Size.Width && this.Position.Left + this.Size.Width >= obj.Position.Left
+        //        && this.Position.Top <= obj.Position.Top + obj.Size.Height && this.Position.Top + this.Size.Height >= obj.Position.Top)
+        //    {
+        //        if (this.Position.Top + this.Size.Height <= obj.Position.Top)
+        //        {
+        //            return Direction.down;
+        //        }
+        //        if (this.Position.Top >= obj.Position.Top + obj.Size.Height)
+        //        {
+        //            return Direction.up;
+        //        }
+
+        //        if (this.Position.Left + this.Size.Width <= obj.Position.Left)
+        //        {
+        //            return Direction.right;
+        //        }
+        //        if (this.Position.Left >= obj.Position.Left + obj.Size.Width)
+        //        {
+        //            return Direction.left;
+        //        }
+        //    }
+
+        //    return Direction.none;
+        //}
     }
 }
